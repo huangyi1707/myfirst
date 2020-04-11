c7cb69283d75ffba79416bdd5ce47b4b53315846
huangyi1707 <18818217118@163.com>
2020-04-11 16:06:08 +0800

    提交测试

--- springboot-01-quick/src/main/java/com/atguigu/springboot01quick/Springboot01QuickApplication.java
+++ springboot-01-quick/src/main/java/com/atguigu/springboot01quick/Springboot01QuickApplication.java
@@ -8,6 +8,7 @@ public class Springboot01QuickApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Springboot01QuickApplication.class, args);
+        System.out.println("");
     }
 
 }
